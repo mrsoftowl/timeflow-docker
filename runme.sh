3a580a1bed851133d400e6c69b2a5e10a8b5d359
mrsoftowl <mrsoftowl@gmail.com>
2018-11-01 08:18:30 -0400

    Update runme.sh
    Added docker run option for 4Gb of memory. This aids in performance.

--- runme.sh
+++ runme.sh
@@ -6,6 +6,7 @@ IP=$(ifconfig en0 |awk /netmask/'{print $2}')
 socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
 
 docker run --rm -it \
+    -m 4000MB \
     -e  DISPLAY=$IP:0 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     timeflow
